8b88e89eb5fd0fb1821e3705a5f6173d0041740b
Joseph Toles <jbtoles@gmail.com>
2015-05-16 14:02:41 -0700

    Added simple search function

--- web/views.py
+++ web/views.py
@@ -8,9 +8,21 @@ def home(request):
 
 
 def search(request):
+    if request.method == 'POST':
+        city = request.POST['city']
+        state = request.POST['state']
+        results_queryset = Hospital.objects
+        if city:
+            results_queryset = results_queryset.filter(city=city)
+        if state:
+            results_queryset = results_queryset.filter(state=state)
+        results = results_queryset.all()
+    else:
+        results = None
+
     states = sorted(set([d['state'] for d in Hospital.objects.values('state')]))
     cities = sorted(set([d['city'] for d in Hospital.objects.values('city')]))
-    context = {'states': states, 'cities': cities}
+    context = {'states': states, 'cities': cities, 'results': results}
     return render(request, 'search.html', context)
 
 
