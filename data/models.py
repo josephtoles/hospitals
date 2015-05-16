8b7e8ee1426ade326f105c5d9e6d4d92cb8ac6d2
Joseph Toles <jbtoles@gmail.com>
2015-05-16 09:46:15 -0700

    Added Hospital model

--- data/models.py
+++ data/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.db.models import IntegerField, CharField, FloatField
 
-# Create your models here.
+
+class hospital(models.Model):
+    provider_id = IntegerField()  # Used by Medicare. Distinct from database primary key.
+    phone_number = IntegerField()
+
+    # Metrics
+    quality = FloatField()
+    atmosphere = FloatField()
+    cost = FloatField()
+
+    # Address
+    name = CharField(max_length=100)
+    address = CharField(max_length=100)
+    city = CharField(max_length=20)
+    State = CharField(max_length=10)
+    zip_code = IntegerField()
+    county_name = IntegerField()
+
