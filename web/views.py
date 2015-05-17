d7356f5cfe58b5d6ff2a76edd30799532b3180eb
Joseph Toles <jbtoles@gmail.com>
2015-05-16 17:08:12 -0700

    Added a little histeresis

--- web/views.py
+++ web/views.py
@@ -8,21 +8,31 @@ def home(request):
 
 
 def search(request):
+    selected_state = None
+    selected_city = None
     if request.method == 'POST':
         city = request.POST['city']
         state = request.POST['state']
         results_queryset = Hospital.objects
         if city:
             results_queryset = results_queryset.filter(city=city)
+            selected_city = city
         if state:
             results_queryset = results_queryset.filter(state=state)
+            selected_state = state
         results = results_queryset.all()
     else:
         results = None
 
     states = sorted(set([d['state'] for d in Hospital.objects.values('state')]))
     cities = sorted(set([d['city'] for d in Hospital.objects.values('city')]))
-    context = {'states': states, 'cities': cities, 'results': results}
+    context = {
+        'states': states,
+        'cities': cities,
+        'results': results,
+        'selected_state': selected_state,
+        'selected_city': selected_city,
+    }
     return render(request, 'search.html', context)
 
 
