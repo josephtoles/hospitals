0ef2c4d6e00fe14971a79f5230d43b5339d2d924
Joseph Toles <jbtoles@gmail.com>
2015-05-17 15:28:26 -0700

    Added letters to map

--- static/js/location_template.js
+++ static/js/location_template.js
@@ -1,13 +1,13 @@
-var siberia = new google.maps.LatLng(37.625364,-122.423905);
-var siberia2 = new google.maps.LatLng(38.625364,-123.423905);
-
 function initialize() {
 
   var myOptions = {
-    zoom:19,
+    zoom:8,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+  var siberia = new google.maps.LatLng(37.625364,-122.423905);
+  /*
 
   var marker =  new google.maps.Marker({
     position: siberia,
@@ -22,37 +22,57 @@ function initialize() {
   google.maps.event.addListener(marker, 'click', function() {
     infowindow.open(map,marker);
   });
+  */
 
-  var marker2 =  new google.maps.Marker({
-    position: siberia2,
-    map: map,
-    title: "omt"
+  /*
+  var mapLabel = new google.maps.MapLabel({
+         text: 'Test',
+         position: new google.maps.LatLng(50,50),
+         map: map,
+         fontSize: 20,
+         align: 'right'
   });
-  map.setCenter(siberia);
+  */
 
-  console.log('mark 1');
+  map.setCenter(siberia);
   var xobj = new XMLHttpRequest();
   xobj.overrideMimeType("application/json");
   xobj.open('GET', 'data/get_json', true);
-  console.log('mark 2');
   xobj.onreadystatechange = function () {
-      console.log('mark 2.5');
       if (xobj.readyState == 4) {
           var jsonTexto = xobj.responseText;
           var list_from_json = JSON.parse( jsonTexto );
           console.log(list_from_json[0]);
 
           for(var i=0; i<list_from_json.length; i++) {
-              var marker = new google.maps.Marker({
-                  position: new google.maps.LatLng(list_from_json[i]['lat'], list_from_json[i]['lng']),
-                  map: map,
-                  title: list_from_json[i]['title']
+              if (i >= 0 && i < 26)
+              {
+                  var letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[i];
+                  var marker = new google.maps.Marker({
+                      position: new google.maps.LatLng(list_from_json[i]['lat'], list_from_json[i]['lng']),
+                      map: map,
+                      title: 'tooltip text',
+                      icon: "http://maps.google.com/mapfiles/marker" + letter + ".png"
+                  });
+              }
+              else {
+                  var marker = new google.maps.Marker({
+                      position: new google.maps.LatLng(list_from_json[i]['lat'], list_from_json[i]['lng']),
+                      map: map,
+                      title: 'tooltip text',
+                  });
+              }
+              /*
+              google.maps.event.addListener(marker, 'click', function () {
+                var infowindow = new google.maps.InfoWindow({ content: 'hospital info' });
+                infowindow.open(map, marker);
               });
+              */
           }
+
       }
   };
   xobj.send(null);
-  console.log('mark 3');
 
 }
 google.maps.event.addDomListener(window, 'load', initialize);
