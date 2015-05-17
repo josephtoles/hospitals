4ca16fe98e3b7cd1edd1a7f1adcb1bcc10160486
Joseph Toles <jbtoles@gmail.com>
2015-05-17 13:56:33 -0700

    Javascript from JSON data works

--- static/js/location_template.js
+++ static/js/location_template.js
@@ -2,6 +2,7 @@ var siberia = new google.maps.LatLng(37.625364,-122.423905);
 var siberia2 = new google.maps.LatLng(38.625364,-123.423905);
 
 function initialize() {
+
   var myOptions = {
     zoom:19,
     mapTypeId: google.maps.MapTypeId.ROADMAP
@@ -29,5 +30,29 @@ function initialize() {
   });
   map.setCenter(siberia);
 
+  console.log('mark 1');
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open('GET', 'data/get_json', true);
+  console.log('mark 2');
+  xobj.onreadystatechange = function () {
+      console.log('mark 2.5');
+      if (xobj.readyState == 4) {
+          var jsonTexto = xobj.responseText;
+          var list_from_json = JSON.parse( jsonTexto );
+          console.log(list_from_json[0]);
+
+          for(var i=0; i<list_from_json.length; i++) {
+              var marker = new google.maps.Marker({
+                  position: new google.maps.LatLng(list_from_json[i]['lat'], list_from_json[i]['lng']),
+                  map: map,
+                  title: list_from_json[i]['title']
+              });
+          }
+      }
+  };
+  xobj.send(null);
+  console.log('mark 3');
+
 }
-  google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, 'load', initialize);
