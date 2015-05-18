77bdfcd1a21d59d31e38e760e8b718c2aa877f8b
Joseph Toles <jbtoles@gmail.com>
2015-05-17 21:04:53 -0700

    Added package configuration

--- hospitals/settings.py
+++ hospitals/settings.py
@@ -76,12 +76,28 @@ WSGI_APPLICATION = 'hospitals.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'aa1wom6z8e493d6',
+            'USER': 'master',
+            'PASSWORD': 'bedpennypeacockkiss',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
+    }
 
 
 # Internationalization
